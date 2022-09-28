12555859ee20e6844214032fc3f0c844887541e3
Megan Johnson <57232347+IamMegan@users.noreply.github.com>
2022-09-28 09:01:24 -0400

    Add files via upload

---
+++ repDigit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    int digitOccurence[10] = {0}; 
+    int num;
+    printf("Enter a number to process: ");
+    scanf("%d", &num);
+    
+    int nDigits = floor(log10(abs(num))) + 1;
+    
+    for(int i = 0; i < nDigits; i++){
+        digitOccurence[(num%10)]++; //this works??????????
+        num /= 10;
+    }
+    
+    printf("Number of occurences: ");
+    
+    for(int i = 0; i < 10; i++){
+        printf("%d ", digitOccurence[i]);
+    }
+    
+    
+    return 0;
+}
