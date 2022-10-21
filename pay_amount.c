17e70c34ec6f0a52edfe6f47bc20f492d8c6cd8d
Megan Johnson <57232347+IamMegan@users.noreply.github.com>
2022-10-21 18:16:38 -0400

    Update pay_amount.c

--- pay_amount.c
+++ pay_amount.c
@@ -2,12 +2,13 @@
 
 
 
-void pay_amout(int dollars, int *twenties, int *tens, int *fives, int *ones);
+void pay_amout(int, int*, int*, int*,int*);
 
 int main()
 {
-    int twenties, tens, fives, ones = 0;
-    pay_amout(0, &twenties, &tens, &fives, &ones);
+    int dollars, twenties, tens, fives, ones;
+    dollars = 60;
+    pay_amout(dollars, &twenties, &tens, &fives, &ones);
     printf("twenties: %d\ntens: %d\nfives: %d\nones: %d\n", twenties, tens, fives, ones);
 
     return 0;
@@ -17,20 +18,25 @@ void pay_amout(int dollars, int *twenties, int *tens, int *fives, int *ones){
     
     while(dollars > 0){
         if(dollars >= 20){
-            *twenties++;
             dollars -= 20;
+            (*twenties)++;
+            //*twenties = (*twenties) + 1;
+        
         }
         else if(dollars >= 10){
-            *tens++;
             dollars -= 10;
+            *tens = (*tens) + 1;
+           
         }
         else if(dollars >= 5){
-            *fives++;
             dollars -= 5;
+            *fives = (*fives) + 1;
+            
         }
-        else{
-            *ones++;
+        else if(dollars>=1){
             dollars -=1;
+            *ones = (*ones) + 1;
+            
         }
     }
     
