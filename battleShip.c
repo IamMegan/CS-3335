3faf2fdf6292e47107a25b4bb32757fb31850e26
Megan Johnson <57232347+IamMegan@users.noreply.github.com>
2022-10-24 13:40:11 -0400

    Create battleShip.c

---
+++ battleShip.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+void setBoard(int *);
+
+void setComputerBoard(int *);
+
+int playGame(int *, int *);
+
+int main()
+{
+    
+    
+    return 0;
+}
